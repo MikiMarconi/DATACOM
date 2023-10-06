d030533a8365872ebff906280a848b0dda1d8f82
Michelangelo <mikimarconi2001@gmail.com>
2023-10-06 13:57:15 +0200

    interfaccia grafica e popolamento tabella

--- ConnectionSSH.py
+++ ConnectionSSH.py
@@ -1,47 +1,36 @@
 from netmiko import ConnectHandler
-import time
-import InterfaceObj
+from InterfaceObj import Interface  # Assicurati di importare la classe Interface da InterfaceObj
 
-interfacce = []
+class Connection:
+    @staticmethod
+    def splitOutput(host, port, username, password, device_type, port1, port2):
+        try:
+            connection = ConnectHandler(host=host, port=port, username=username, password=password,
+                                        device_type=device_type)
+            output = connection.send_command('display interface brief')
+            # Dividi l'output in righe
+            linee = output.strip().split('\n')
+            interfacce = []
 
-def splitOutput(output):
-    # Dividi l'output in righe
-    linee = output.strip().split('\n')
-    interfacce = []
+            # Itera attraverso le righe per cercare le interfacce desiderate
+            for linea in linee:
+                colonne = linea.split()
+                if len(colonne) >= 1 and colonne[0] in ["GigabitEthernet0/0/0." + port1,
+                                                        "GigabitEthernet0/0/0." + port2]:
+                    interfaccia = colonne[0]
+                    phy = colonne[1]
+                    protocol = colonne[2]
+                    inUti = colonne[3]
+                    outUti = colonne[4]
+                    inErrors = colonne[5]
+                    outErrors = colonne[6]
 
-    # Itera attraverso le righe per cercare le interfacce desiderate
-    for linea in linee:
-        colonne = linea.split()
-        if len(colonne) >= 1 and colonne[0] in ["GigabitEthernet0/0/0.51", "GigabitEthernet0/0/0.52"]:
-            interfaccia = colonne[0]
-            phy = colonne[1]
-            protocol = colonne[2]
-            inUti = colonne[3]
-            outUti = colonne[4]
-            inErrors = colonne[5]
-            outErrors = colonne[6]
+                    # Crea un oggetto Interface
+                    interfaccia_obj = Interface(interfaccia, phy, protocol, inUti, outUti, inErrors, outErrors)
+                    interfacce.append(interfaccia_obj)
 
-            # Crea un oggetto Interface
-            interfaccia_obj = InterfaceObj.Interface(interfaccia, phy, protocol, inUti, outUti, inErrors, outErrors)
-            interfacce.append(interfaccia_obj)
+            connection.disconnect()
+            return interfacce
 
-    return interfacce
-
-while True:
-    try:
-        connection = ConnectHandler(host='10.100.0.9', port='22', username='admin', password='huawei123', device_type='huawei_vrp')
-        output = connection.send_command('display interface brief')
-        interfacce = splitOutput(output)
-        interfaccia51 = interfacce[0]
-        interfaccia52 = interfacce[1]
-        print(interfaccia52.interface)
-        connection.disconnect()
-        # Attendere 60 secondi prima della successiva iterazione
-        time.sleep(60)
-
-
-    except Exception as e:
-        print(f"Errore: {str(e)}")
-
-
-
+        except Exception as e:
+            print(f"Errore: {str(e)}")
